0e74ef5da1bc6cb78ff4a6a6d38fbf371e83e8ed
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-11-09 20:38:56 -0800

    bug/NoPageFoundOnRefresh: Fix for no page found error on refresh page

--- FrontEnd/src/index.jsx
+++ FrontEnd/src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './store';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <Provider store={store}>
+  <Provider store={store}>
+    <Router>
       <App />
-    </Provider>
-  </Router>,
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
