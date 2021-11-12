dcba4bcb2c7889a8a8500828779007ccb9a7577a
akshay-26 <akshaykulkarni.bk20@gmail.com>
2021-11-11 18:16:11 -0800

    added user register backend

---
+++ Middleware/DbConn/DbConnection.js
@@ -0,0 +1,18 @@
+// mysql = require('mysql');
+// var db;
+
+// function dbConnectionProvider() {
+//     if (!db) {
+//         db = mysql.createPool({
+//             connectionLimit: 10,
+//             host: "indeed.crmxoy9swngf.us-east-2.rds.amazonaws.com",
+//             port: 3306,
+//             user: "admin",
+//             password: "Indeed273",
+//             database: "indeed"
+//         });
+//     }
+//     return db;
+
+// }
+// module.exports = dbConnectionProvider();
