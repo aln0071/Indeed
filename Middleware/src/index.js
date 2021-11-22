68c65b96d7aa897b4c3346670b70d51af58f8d36
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-11-22 13:13:45 -0800

    Middleware: Changes for registering user

--- Middleware/src/index.js
+++ Middleware/src/index.js
@@ -1,23 +1,28 @@
-/* eslint-disable */
-// const mysql = require("mysql");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const UserRegistration = require("./Registration/UserRegistration");
+// import helmet for extra security
+const helmet = require('helmet');
+
+// import morgan for loging
+const morgan = require('morgan');
+
+const UserRegistration = require('./Registration/UserRegistration');
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const review = require("./Reviews/Reviews");
+const review = require('./Reviews/Reviews');
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+// add Helmet for extra security
+app.use(helmet());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+app.use(cors());
 
 // var corsOptions = {  //eslint-disable-line
 //     origin: "http://localhost:3000" //eslint-disable-line
@@ -28,14 +33,14 @@ app.use(
 //     console.log("connected");
 // })
 
-app.use("/indeed/api", review);
-app.use("/indeed/user", UserRegistration);
+app.use('/indeed/api', review);
+app.use('/indeed/user', UserRegistration);
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get('/', (req, res) => {
+  res.send('hello');
 });
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`); //eslint-disable-line
