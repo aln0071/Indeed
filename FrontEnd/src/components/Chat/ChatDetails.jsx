4d84e9e42c15b7225c353f3a78928c5d674d455c
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-11-28 14:53:26 -0800

    feature/ChatCSS: css changes for chat

--- FrontEnd/src/components/Chat/ChatDetails.jsx
+++ FrontEnd/src/components/Chat/ChatDetails.jsx
@@ -5,12 +5,18 @@ import { Box } from '@material-ui/core';
 import ChatDetailsHeader from './ChatDetailsHeader';
 import ChatConversation from './ChatConversation';
 import ChatDetailsFooter from './ChatDetailsFooter';
+import styles from '../../styles.scss';
 
 function ChatDetails() {
   return (
-    <Box display="flex" flexDirection="column" height="100%">
+    <Box
+      display="flex"
+      flexDirection="column"
+      maxHeight="100%"
+      className={styles.chatBody}
+    >
       <ChatDetailsHeader />
-      <Box p={3} height="100%" style={{ overflowY: 'auto' }}>
+      <Box p={3} maxHeight="100%" style={{ overflowY: 'auto' }}>
         <ChatConversation />
       </Box>
       <ChatDetailsFooter />
