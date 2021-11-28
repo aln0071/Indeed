fb7caffe838c715df2fa9f20c15fafed362151a2
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-11-28 11:45:18 -0800

    feature/NavbarAndSignout: Navbar with dropdown added; Signout implemented;

--- FrontEnd/src/store/middleware.js
+++ FrontEnd/src/store/middleware.js
@@ -1,5 +1,15 @@
-/* eslint no-unused-vars: 0 */
+import { CLEAR_USER_DETAILS, SET_USER_DETAILS } from './actions/types';
+
+/* eslint-disable */
 const middleware = (store) => (next) => (action) => {
+  if (action.type === SET_USER_DETAILS) {
+    window.sessionStorage.setItem(
+      "user",
+      JSON.stringify({ ...store.getState("user").user, ...action.payload })
+    );
+  } else if (action.type === CLEAR_USER_DETAILS) {
+    window.sessionStorage.removeItem("user");
+  }
   next(action);
 };
 
