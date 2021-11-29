5706979ec072bf2e8c1ccb52abe744a11b0d5f80
Suharsh <cksuharsh@gmail.com>
2021-11-28 20:02:59 -0800

    review endpoints

--- Database/src/services/reviews/GetReviews.js
+++ Database/src/services/reviews/GetReviews.js
@@ -1,9 +1,56 @@
 const Reviews = require('../../model/Review');
 
 async function handleRequest(req, callback) {
+  let reviews = {};
+  let error = {};
   const { companyId } = req.params;
-  const reviews = await Reviews.find({ companyId });
-  callback(null, reviews);
+  const { sort } = req.query;
+  const { order } = req.query;
+  let sortCriteria = {};
+  let sortOrder;
+  if (order === 'desc') {
+    sortOrder = -1;
+  } else {
+    sortOrder = 1;
+  }
+  if (sort === 'rating') {
+    sortCriteria = { rating: parseInt(sortOrder, 10) };
+  } else if (sort === 'date') {
+    sortCriteria = { reviewDate: parseInt(sortOrder, 10) };
+  } else if (sort === 'helpful') {
+    sortCriteria = { helpfulnessPositive: parseInt(sortOrder, 10) };
+  }
+  try {
+    if (
+      req.query.page
+      && req.query.limit
+      && req.query.sort
+      && req.query.order
+    ) {
+      const page = parseInt(req.query.page, 10);
+      const limit = parseInt(req.query.limit, 10);
+      const skipIndex = (page - 1) * limit;
+      reviews = await Reviews.find({ companyId })
+        .sort(sortCriteria)
+        .limit(limit)
+        .skip(skipIndex);
+    } else if (req.query.sort && req.query.order) {
+      reviews = await Reviews.find({ companyId }).sort(sortCriteria);
+    } else if (req.query.page && req.query.limit) {
+      const page = parseInt(req.query.page, 10);
+      const limit = parseInt(req.query.limit, 10);
+      const skipIndex = (page - 1) * limit;
+      reviews = await Reviews.find({ companyId }).limit(limit).skip(skipIndex);
+    } else if (req.query.page || req.query.limit) {
+      error = { message: 'Pass both page and limit and not just one' };
+      callback(error, null);
+    } else {
+      reviews = await Reviews.find({ companyId });
+    }
+    callback(null, reviews);
+  } catch (err) {
+    callback(err, null);
+  }
 }
 
 exports.handle_request = handleRequest;
