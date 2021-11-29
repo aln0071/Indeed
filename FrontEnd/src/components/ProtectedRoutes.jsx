9b711d8a8907abb24d608612e9f8a1eef932b9f7
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-11-29 11:04:57 -0800

    feature/ProtectedRoutes: bug fix

--- FrontEnd/src/components/ProtectedRoutes.jsx
+++ FrontEnd/src/components/ProtectedRoutes.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
 export default function ProtectedRoute({ path, component }) {
-  if (useSelector((state) => state.user.userid)) {
+  if (useSelector((state) => state.user.userId)) {
     return <Route exact path={path} component={component} />;
   }
   return <Redirect to="/login" />;
