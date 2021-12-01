63ddeb8e6e37f92060c09cf4583b48d3c009c170
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-12-01 08:47:21 -0800

    Update kafka setup script

--- kafkaSetup.sh
+++ kafkaSetup.sh
@@ -4,7 +4,8 @@ if [[ $KAFKA_HOME != *"kafka_2.13-3.0.0" ]]; then
   echo "Please set correct kafka home path inside this script on line 2"
 else
     cd $KAFKA_HOME
-    for TOPIC in indeed_create_message indeed_post_helpful_reviews indeed_get_messages indeed_create_room indeed_get_rooms indeed_put_company indeed_get_company indeed_apply_job indeed_get_reviews indeed_add_jobseekerInfo_salary indeed_get_all_company indeed_get_jobs indeed_get_reviews indeed_post_reviews response_topic indeed_get_all_job_locations indeed_post_job indeed_post_company indeed_get_all_jobs indeed_get_job indeed_get_jobseeker_profile indeed_userprofile_update; do
-    bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic $TOPIC
+    for TOPIC in indeed_create_message indeed_get_user_reviews indeed_getapplied_job indeed_get_user_profile indeed_get_companyDetails indeed_get_companyDetails_by_employerId indeed_add_pictures indeed_post_helpful_reviews indeed_get_messages indeed_create_room indeed_get_rooms indeed_put_company indeed_get_company indeed_apply_job indeed_get_reviews indeed_add_jobseekerInfo_salary indeed_get_all_company indeed_get_jobs indeed_get_reviews indeed_post_reviews response_topic indeed_get_all_job_locations indeed_post_job indeed_post_company indeed_get_all_jobs indeed_get_job indeed_get_jobseeker_profile indeed_userprofile_update; do
+    bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic $TOPIC &
     done
 fi
+wait
