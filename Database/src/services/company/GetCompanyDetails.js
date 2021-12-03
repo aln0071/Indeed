775d95f1f0268c6c4acb60478e52439ab18f0914
Suharsh <cksuharsh@gmail.com>
2021-12-03 03:40:21 -0800

    update company details, saved jobs

--- Database/src/services/company/GetCompanyDetails.js
+++ Database/src/services/company/GetCompanyDetails.js
@@ -1,15 +1,29 @@
 /* eslint-disable linebreak-style */
 const Company = require('../../model/Company');
+const Reviews = require('../../model/Review');
 
 // To get company details
 async function handleRequest(req, callback) {
   try {
+    const { companyId } = req.params;
     const company = await Company.findOne({
       companyId: req.params.companyId,
     }).populate('pictures');
-
-    // const savedCompany = await company.save();
-    callback(null, company);
+    const averageRating = [
+      { $match: { companyId } },
+      {
+        $group: {
+          _id: null,
+          averageRating: { $avg: '$rating' },
+          averageLearning: { $avg: '$learning' },
+          averageAppreciation: { $avg: '$appreciation' },
+        },
+      },
+      { $project: { _id: 0 } },
+    ];
+    const rating = await Reviews.aggregate(averageRating);
+    const response = { ...company.toJSON(), averageRatings: rating[0] };
+    callback(null, response);
   } catch (error) {
     callback(error, null);
   }
--- Middleware/src/routes/jobs/Jobs.js
+++ Middleware/src/routes/jobs/Jobs.js
@@ -1,17 +1,17 @@
 /* eslint-disable consistent-return */
 /* eslint-disable linebreak-style */
-const router = require("express").Router();
-const kafka = require("../../kafka/Client");
-const redisCli = require("../../redis/Connection");
+const router = require('express').Router();
+const kafka = require('../../kafka/Client');
+const redisCli = require('../../redis/Connection');
 
-router.post("/company/:companyId/jobs", async (req, res) => {
+router.post('/company/:companyId/jobs', async (req, res) => {
   const request = {
     query: req.query,
     params: req.params,
     body: req.body,
   };
   const companyJobsRedisUrl = `/company/${req.params.companyId}/jobs`;
-  kafka.make_request("indeed_post_job", request, (error, results) => {
+  kafka.make_request('indeed_post_job', request, (error, results) => {
     if (error) {
       res.status(400).send(error);
     } else {
@@ -19,7 +19,7 @@ router.post("/company/:companyId/jobs", async (req, res) => {
         redisCli.setex(
           companyJobsRedisUrl,
           3600,
-          JSON.stringify({ jobs: results.allCompanyJobs })
+          JSON.stringify({ jobs: results.allCompanyJobs }),
         );
       }
       res.status(200).send(results.data);
@@ -27,7 +27,7 @@ router.post("/company/:companyId/jobs", async (req, res) => {
   });
 });
 
-router.get("/company/:companyId/jobs", async (req, res) => {
+router.get('/company/:companyId/jobs', async (req, res) => {
   const request = {
     query: req.query,
     params: req.params,
@@ -44,8 +44,7 @@ router.get("/company/:companyId/jobs", async (req, res) => {
         const limit = parseInt(req.query.limit, 10) || 5;
         const slicedData = allJobs.slice((page - 1) * limit, page * limit);
         const totalCount = allJobs.length;
-        const noOfPagesLeft =
-          totalCount <= limit ? 0 : Math.ceil(totalCount / limit - page);
+        const noOfPagesLeft = totalCount <= limit ? 0 : Math.ceil(totalCount / limit - page);
         const totalPages = Math.ceil(totalCount / limit);
         metadata = {
           noOfPagesLeft,
@@ -55,12 +54,12 @@ router.get("/company/:companyId/jobs", async (req, res) => {
         return res.status(200).send({ metadata, reviews: slicedData });
       }
       if (req.query.page || req.query.limit) {
-        const error = { message: "Pass both page and limit and not just one" };
+        const error = { message: 'Pass both page and limit and not just one' };
         return res.status(400).send(error);
       }
       return res.status(200).send(allJobs);
     }
-    kafka.make_request("indeed_get_jobs", request, (error, results) => {
+    kafka.make_request('indeed_get_jobs', request, (error, results) => {
       if (error) {
         return res.status(400).send(error);
       }
@@ -68,7 +67,7 @@ router.get("/company/:companyId/jobs", async (req, res) => {
         redisCli.setex(
           redisUrl,
           3600,
-          JSON.stringify({ jobs: results.allJobs })
+          JSON.stringify({ jobs: results.allJobs }),
         );
       }
       return res.status(200).send(results.data);
@@ -76,14 +75,14 @@ router.get("/company/:companyId/jobs", async (req, res) => {
   });
 });
 
-router.get("/jobs", async (req, res) => {
+router.get('/jobs', async (req, res) => {
   const request = {
     query: req.query,
     params: req.params,
     body: req.body,
   };
 
-  kafka.make_request("indeed_get_all_jobs", request, (error, results) => {
+  kafka.make_request('indeed_get_all_jobs', request, (error, results) => {
     if (error) {
       res.status(400).send(error);
     } else {
@@ -92,7 +91,7 @@ router.get("/jobs", async (req, res) => {
   });
 });
 
-router.get("/jobs/locations", async (req, res) => {
+router.get('/jobs/locations', async (req, res) => {
   const request = {
     query: req.query,
     params: req.params,
@@ -100,7 +99,7 @@ router.get("/jobs/locations", async (req, res) => {
   };
 
   kafka.make_request(
-    "indeed_get_all_job_locations",
+    'indeed_get_all_job_locations',
     request,
     (error, results) => {
       if (error) {
@@ -108,18 +107,18 @@ router.get("/jobs/locations", async (req, res) => {
       } else {
         res.status(200).send(results);
       }
-    }
+    },
   );
 });
 
-router.get("/company/:companyId/jobs/:jobId", async (req, res) => {
+router.get('/company/:companyId/jobs/:jobId', async (req, res) => {
   const request = {
     query: req.query,
     params: req.params,
     body: req.body,
   };
 
-  kafka.make_request("indeed_get_job", request, (error, results) => {
+  kafka.make_request('indeed_get_job', request, (error, results) => {
     if (error) {
       res.status(400).send(error);
     } else {
@@ -133,28 +132,28 @@ router.get("/company/:companyId/jobs/:jobId", async (req, res) => {
 });
 
 router.post(
-  "/jobseeker/:jobseekerId/applyJob/:jobId/:companyId",
+  '/jobseeker/:jobseekerId/applyJob/:jobId/:companyId',
   async (req, res) => {
     const request = {
       params: req.params,
       body: req.body,
     };
-    kafka.make_request("indeed_apply_job", request, (error, results) => {
+    kafka.make_request('indeed_apply_job', request, (error, results) => {
       if (error) {
         res.status(400).send(error);
       } else {
         res.status(200).send(results);
       }
     });
-  }
+  },
 );
 
-router.get("/jobseeker/getAppliedJob/:jobseekerId", async (req, res) => {
+router.get('/jobseeker/getAppliedJob/:jobseekerId', async (req, res) => {
   const request = {
     params: req.params,
     body: req.body,
   };
-  kafka.make_request("indeed_getapplied_job", request, (error, results) => {
+  kafka.make_request('indeed_getapplied_job', request, (error, results) => {
     if (error) {
       res.status(400).send(error);
     } else {
@@ -163,28 +162,13 @@ router.get("/jobseeker/getAppliedJob/:jobseekerId", async (req, res) => {
   });
 });
 
-router.post("/jobseeker/:jobseekerId/saveJob", async (req, res) => {
-  const request = {
-    params: req.params,
-    body: req.body,
-  };
-  console.log(request);
-  kafka.make_request("indeed_post_save_job", request, (error, results) => {
-    if (error) {
-      res.status(400).send(error);
-    } else {
-      res.status(200).send(results);
-    }
-  });
-});
-
-router.post("/jobseeker/:jobseekerId/undoSaveJob", async (req, res) => {
+router.post('/jobseeker/:jobseekerId/saveJob', async (req, res) => {
   const request = {
     params: req.params,
     body: req.body,
   };
   console.log(request);
-  kafka.make_request("indeed_post_undosave_job", request, (error, results) => {
+  kafka.make_request('indeed_post_save_job', request, (error, results) => {
     if (error) {
       res.status(400).send(error);
     } else {
@@ -193,12 +177,41 @@ router.post("/jobseeker/:jobseekerId/undoSaveJob", async (req, res) => {
   });
 });
 
-router.get("/salaries/jobs", async (req, res) => {
+router.post('/jobseeker/:jobseekerId/undoSaveJob', async (req, res) => {
+  const request = {
+    params: req.params,
+    body: req.body,
+  };
+  console.log(request);
+  kafka.make_request('indeed_post_undosave_job', request, (error, results) => {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+router.get('/salaries/jobs', async (req, res) => {
   const request = {
     params: req.params,
     query: req.query,
   };
-  kafka.make_request("indeed_get_salaries", request, (error, results) => {
+  kafka.make_request('indeed_get_salaries', request, (error, results) => {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+router.get('/jobseeker/:jobseekerId/savedJobs', async (req, res) => {
+  const request = {
+    params: req.params,
+    query: req.query,
+  };
+  kafka.make_request('indeed_get_saved_jobs', request, (error, results) => {
     if (error) {
       res.status(400).send(error);
     } else {
